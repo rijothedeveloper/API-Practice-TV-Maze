fda744b37affab7be6ea3476172c09e95eede47f
Rijo George <rijothedeveloper@gmail.com>
2021-11-22 08:44:03 -0800

    Step 3: Make AJAX request for Search

--- tvmaze.js
+++ tvmaze.js
@@ -17,18 +17,30 @@
         image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
       }
  */
-async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+// Base url for api
+const BASE_URL = "http://api.tvmaze.com/";
+
+async function searchShows(query) {
+  // end point for search
+  const end_point = "search/shows"
+  const res = await axios.get(BASE_URL+end_point, {params: {"q": query}});
+  console.log(res);
+  const shows = [];
+  
+  for (item of res.data) {
+    const show = {};
+    try {
+      show.id = item.show.id;
+      show.name = item.show.name;
+      show.summary = item.show.summary;
+      show.image = item.show.image.medium;
+    } catch {
+
     }
-  ]
+    shows.push(show);
+  }
+  return shows;
 }
 
 
